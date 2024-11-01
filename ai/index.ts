25fde769e7b5f91eca30c608dc98acf802e57fab
hamster1963 <1410514192@qq.com>
2024-11-01 09:09:39 +0800

    fix: custom env

--- ai/index.ts
+++ ai/index.ts
@@ -1,7 +1,12 @@
-import { openai } from '@ai-sdk/openai';
+import {  createOpenAI } from '@ai-sdk/openai';
 import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
 
 import { customMiddleware } from './custom-middleware';
+
+const openai = createOpenAI({
+  baseURL: process.env.OPENAI_BASE_URL,
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 export const customModel = (apiIdentifier: string) => {
   return wrapLanguageModel({
